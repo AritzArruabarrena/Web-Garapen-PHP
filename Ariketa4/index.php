fbcef3a1180b9476612592183a1e570ba2b58ff0
Aritz <arruabarrena.aritz@uni.eus>
2024-11-20 13:27:09 +0100

    ariketa4 hasiera

---
+++ Ariketa4/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ariketa 4</title>
+</head>
+<body>
+    <p>Etxebizitzaren prezioa igotzen jarraituko duela uste duzu?</p>
+</body>
+</html>
